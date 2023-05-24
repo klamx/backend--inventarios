5dab855f56f9cff03078b3984cb47249bf315330
Alejandro Largo <largoalejo@gmail.com>
2023-05-24 00:59:00 -0500

    jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require('express')
 const bcrypt = require('bcryptjs')
 const { User } = require('../models/User')
 const { sequelize } = require('../database/config')
+const { generateJWT } = require('../helpers/jwt')
 
 const createUser = async (req, res = response) => {
   const { username, email, password, is_admin } = req.body
@@ -36,12 +37,16 @@ const createUser = async (req, res = response) => {
       is_admin,
     })
 
+    // Generar JWT
+    const token = await generateJWT(createUser.user_id, createUser.username)
+
     return res.status(201).json({
       ok: true,
       msg: 'registro',
       username,
       email,
       id: createUser.user_id,
+      token,
     })
   } catch (error) {
     res.status(500).json({
@@ -55,42 +60,44 @@ const loginUser = async (req, res = response) => {
   const { email, password } = req.body
 
   try {
-    const user = await User.findOne({where: {
-      email
-    }})
+    const user = await User.findOne({
+      where: {
+        email,
+      },
+    })
 
     if (!user) {
       return res.status(400).json({
         ok: false,
-        msg: "Usuario o contraseñá incorrectos"
+        msg: 'Usuario o contraseñá incorrectos',
       })
     }
 
     // validar contraseñá
     const validatePassword = bcrypt.compareSync(password, user.password)
-    
+
     if (!validatePassword) {
       return res.status(400).json({
         ok: false,
-        msg: "Usuario o contraseñá incorrectos"
+        msg: 'Usuario o contraseñá incorrectos',
       })
     }
 
     // Generar JWT
+    const token = await generateJWT(user.user_id, user.username)
 
     return res.json({
       ok: true,
       id: user.user_id,
       username: user.username,
+      token,
     })
-
   } catch (error) {
     res.status(500).json({
       ok: false,
       msg: 'Error al loguearse',
     })
   }
-  
 }
 
 const revalidateToken = (req, res = response) => {
